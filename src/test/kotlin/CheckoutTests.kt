03d8d9e34a560ec75614a8495ba7d6bbae3b6f1c
Duncan McGregor <duncan.mcgregor@rbs.com>
2020-11-06 15:24:51 +0000

    Add TravelCardDiscount

--- src/test/kotlin/CheckoutTests.kt
+++ src/test/kotlin/CheckoutTests.kt
@@ -3,7 +3,7 @@ import kotlin.test.assertEquals
 
 class CheckoutTests {
 
-    private val item1 = Item("001", "Travel Card Holder", 9.95)
+    private val item1 = Item("001", "Travel Card Holder", 9.25)
     private val item2 = Item("002", "Personalised cufflinks", 45.00)
     private val item3 = Item("003", "Kids T-shirt", 19.95)
 
@@ -13,7 +13,7 @@ class CheckoutTests {
         co.scan(item1)
         co.scan(item2)
         co.scan(item3)
-        assertEquals(74.90, co.total())
+        assertEquals(74.20, co.total())
     }
 
     @Test
@@ -25,7 +25,27 @@ class CheckoutTests {
         co.scan(item2)
         assertEquals(81.00, co.total())
     }
+
+    @Test
+    fun `cut price travel card holder`() {
+        val co = Checkout(TravelCardDiscount)
+        co.scan(item1)
+        assertEquals(9.25, co.total())
+
+        co.scan(item1)
+        assertEquals(17.00, co.total())
+    }
 }
+
+object TravelCardDiscount : (List<Item>) -> Double {
+    override fun invoke(items: List<Item>): Double {
+        val travelCardCount = items.count { it.code == "001" }
+        val travelCardDiscount = if (travelCardCount > 1) travelCardCount * 0.75 else 0.0
+        val base = items.sumByDouble { it.price.toDouble() }
+        return base - travelCardDiscount
+    }
+}
+
 
 object BasketTotalDiscount : (List<Item>) -> Double {
     override fun invoke(items: List<Item>): Double {
